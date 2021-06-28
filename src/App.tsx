2ad5fd184549ae03deb6108d515dcd7fb3b76e6f
Manntrix <Manish.mandal21@gmail.com>
2021-06-28 18:18:13 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">gdgdgdggdgdgdg</header>
+    </div>
+  );
+}
+
+export default App;
